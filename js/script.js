337987eb97181e836140ddc709ba99e9a5e3ad18
LucioGhedo <lucioghedina@gmail.com>
2022-06-23 14:49:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            objectName: {
+                helloVue: 'Hello Vue.js',
+                name: 'Lucio Ghedina',
+                age: 22,
+            },
+            bonusObject: {
+                img: 'https://i2.wp.com/www.linkiesta.it/wp-content/uploads/2020/02/dd998222-599c-4aa4-a8b4-49f0feaf1268_large.jpg?fit=1200%2C874&ssl=1',
+                imgName: 'scimmie'
+            }
+        }
+    }
+);
